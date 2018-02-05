7ad61a8815edc2779ee25696173d62478653a96c
jlrau <jonathonrau@gmail.com>
2018-02-05 11:56:34 +0000

    Added Comments

--- xml_parser.py
+++ xml_parser.py
@@ -1,20 +1,26 @@
+#! /usr/bin/python
+
 import urllib.request
 import xml.etree.ElementTree as etree
 import csv
 
+# Set Variables
 url = 'https://www.w3schools.com/xml/cd_catalog.xml'
 source_element = 'CD'
 target_file = 'xml_data.csv'
 
+#Pull xml from web address, including proxy handling
 urllib.request.ProxyHandler()
 page = urllib.request.urlopen(url)
 xml_content = page.read()
 e = etree.fromstring(xml_content)
 
+#Setup target xml file
 xml_data = open(target_file, 'w')
 csvwriter = csv.writer(xml_data)
 xml_head = []
 
+#Final all attributes of "source_element" and write to CSV file ("target_file")
 count = 0
 for member in e.findall(source_element):
     xml_row = []
